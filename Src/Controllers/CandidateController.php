2e5ceb15e10166e84188d0a012871ebf11616d79
zahrae028 <fatimabk028@gmail.com>
2026-01-19 00:02:15 +0100

    feat: Establish initial MVC application structure with core routing, user management, and authentication components.

---
+++ Src/Controllers/CandidateController.php
@@ -0,0 +1,8 @@
+<?php
+
+
+class CandidateController{
+    public function dashboard(){}
+}
+
+?>
---
+++ Src/Controllers/RecruiterController.php
@@ -0,0 +1,9 @@
+<?php
+
+
+class RecruiterController
+{
+    public function dashboard(){}
+}
+
+?>
